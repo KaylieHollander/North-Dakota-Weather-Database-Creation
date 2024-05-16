532f0e87343311a402985abf6e190c4e049e32a7
Kaylie <kphollander1@gmail.com>
2024-05-16 13:26:47 -0700

    Created a class called instanceVars and added all necessary instance variables

---
+++ WeatherAPI.py
@@ -0,0 +1,21 @@
+class instanceVars:
+    def __init__(self, latitude, longitude, month, day, year, fiveYearAvgTemp, fiveYearMinTemp,
+                 fiveYearMaxTemp, fiveYearAvgWindSpeed, fiveYearMinWindSpeed, fiveYearMaxWindSpeed,
+                 fiveYearRainSum, fiveYearRainMin, fiveYearRainMax):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.month = month
+        self.day = day
+        self.year = year
+        self.fiveYearAvgTemp = fiveYearAvgTemp
+        self.fiveYearMinTemp = fiveYearMinTemp
+        self.fiveYearMaxTemp = fiveYearMaxTemp
+        self.fiveYearAvgWindSpeed = fiveYearAvgWindSpeed
+        self.fiveYearMinWindSpeed = fiveYearMinWindSpeed
+        self.fiveYearMaxWindSpeed = fiveYearMaxWindSpeed
+        self.fiveYearRainSum = fiveYearRainSum
+        self.fiveYearRainMin = fiveYearRainMin
+        self.fiveYearRainMax = fiveYearRainMax
+
+
+
